3a86d041b35a269f93bf8a8d4be9aa2757ea5e51
ines <ines.dahmani@esprit.tn>
2025-01-27 15:01:42 +0100

    Initial commit: Adding tpfoyerdevops project

---
+++ src/main/java/com/example/tpfoyerdevops/TpfoyerdevopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.tpfoyerdevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpfoyerdevopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpfoyerdevopsApplication.class, args);
+    }
+
+}
