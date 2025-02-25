0822840abadd19028f1d9ed5adca81025fb9bd9c
MohamedhediHamdi1 <117778285+MohamedhediHamdi1@users.noreply.github.com>
2025-02-25 21:10:08 +0100

    login commit 2

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.jsx
+import React from 'react';
+import SignUp from './components/SignUp';
+import './index.css';
+
+
+function App() {
+  return (
+    <div>
+      <SignUp />
+    </div>
+  );
+}
+
+export default App;
