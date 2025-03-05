ff49a19af1e881fb9b8a05ac0028485d40025d6a
walidbarhoumi <100834407+walidbarhoumi@users.noreply.github.com>
2025-03-05 16:39:18 +0100

    third commit

--- backend/app.js
+++ backend/app.js
@@ -22,12 +22,6 @@ var mongoconnection = require('./Config/connection.json');
   .then(() => console.log("Connected to DB success!!"))
   .catch(err => console.error("Could not connect to DB", err));
 
-
-
-
-
-
-
 // Middleware
 app.use(cors());
 app.use(express.json());
