f63bc94ccb83e42be6aa1214048f7fdc490b115d
walidbarhoumi <100834407+walidbarhoumi@users.noreply.github.com>
2025-04-07 15:51:39 +0100

    fixingg template

--- backend/Routes/Test.js
+++ backend/Routes/Test.js
@@ -11,6 +11,10 @@ module.exports = router;
 var express = require('express');
 var router = express.Router();
 
+router.get('/', (req, res) => {
+  res.json({ message: 'Test route works!' });
+});
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('ines noussa');
