77287234d4bec83d9885bb1dbc86755308d86dce
MohamedhediHamdi1 <117778285+MohamedhediHamdi1@users.noreply.github.com>
2025-04-16 03:16:32 +0100

    devops 6

---
+++ frontend/vitest.config.js
@@ -0,0 +1,10 @@
+// frontend/vitest.config.js
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    environment: 'jsdom', // <-- This simulates a browser environment
+    globals: true,
+    css: true,
+  },
+})
